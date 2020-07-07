5b6984374eb2537c805d3980b7fdf5ac96c2577c
Eugene Nosenko <eugenenosenko@gmail.com>
2020-07-07 18:12:53 +0200

    feat: created hotel_image.dart with asset image file

---
+++ lib/pages/hotel_onboarding_page/hotel_image.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutterportoflioapp/pages/hotel_onboarding_page/hotel_onboarding_page.dart';
+import 'package:provider/provider.dart';
+
+class HotelImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<PageControllerNotifier>(
+      builder: (context, notifier, child) {
+        return Positioned(
+          top: -300,
+          left: -notifier.offset * 0.15,
+          width: 500,
+          child: Image.asset('assets/images/monocrome-building.jpg'),
+        );
+      },
+    );
+  }
+}
