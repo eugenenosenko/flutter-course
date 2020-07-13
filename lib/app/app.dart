1c809dd69f0d8f7dd4650fceedaf6de1377a81eb
Eugene Nosenko <eugenenosenko@gmail.com>
2020-07-13 16:51:13 +0200

    refactoring: minor changes to hotel_image.dart and app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 // import 'package:flutterportoflioapp/pages/my_animated_switcher/my_animated_switcher.dart';
 
  import 'package:flutterportoflioapp/pages/gmail_page/gmail_page.dart';
+import 'package:flutterportoflioapp/pages/lab_page/lab_page.dart';
 
 class AppWrapper extends StatelessWidget {
   @override
@@ -18,7 +19,7 @@ class _MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
-      home: GmailPage(),
+      home: LabPage(),
     );
   }
 }
--- lib/pages/hotel_onboarding_page/hotel_image.dart
+++ lib/pages/hotel_onboarding_page/hotel_image.dart
@@ -3,6 +3,8 @@ import 'package:flutterportoflioapp/pages/hotel_onboarding_page/hotel_onboarding
 import 'package:provider/provider.dart';
 
 class HotelImage extends StatelessWidget {
+  final String imagePath = 'assets/images/monocrome-building.jpg';
+
   @override
   Widget build(BuildContext context) {
     return Consumer<PageControllerNotifier>(
@@ -11,7 +13,7 @@ class HotelImage extends StatelessWidget {
           top: -300,
           left: -notifier.offset * 0.15,
           width: 500,
-          child: Image.asset('assets/images/monocrome-building.jpg'),
+          child: Image.asset(imagePath),
         );
       },
     );
